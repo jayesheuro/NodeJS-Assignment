84449ca59cb50600f2d0ba505561b65377137f07
Jayesh Singh <jayeshsingh001@gmail.com>
2022-02-02 17:45:20 +0530

    add-model-data

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log("Server is up and running at port ", PORT);
+  console.log("Server is up and running at port", PORT);
 });
---
+++ userData.js
@@ -0,0 +1,30 @@
+DATA = [
+  {
+    username: "jayesh",
+    password: "abcd1234",
+    email: "jayesh@gmail.com",
+    mobile: "9374856564",
+  },
+  {
+    username: "rahul",
+    password: "1234abcd",
+    email: "rahul@gmail.com",
+    mobile: "9897867552",
+  },
+  {
+    username: "abhishek",
+    password: "abc123",
+    email: "abhishek@gmail.com",
+    mobile: "9887766554",
+  },
+  {
+    username: "abhinav",
+    password: "abcde123",
+    email: "abhinav@gmail.com",
+    mobile: "9827766554",
+  },
+];
+
+module.exports = {
+  DATA,
+};
